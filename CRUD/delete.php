73391a0402a13532bd546ab675f9c567ce7e6406
Matheus Liberato <liberatosciolimatheus@gmail.com>
2022-04-12 10:48:17 -0300

    Adicionada a função delete

---
+++ CRUD/delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Source\Models\User;
+use Source\Models\Address;
+
+$user = (new User())->findById(2);
+if ($user) {
+    $address = (new Address())->find("user_id = {$user->id}")->fetch(false);
+    $address->destroy();
+    $user->destroy();
+} else {
+    var_dump($user);
+}
